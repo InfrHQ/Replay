9e9ae26973001bde1ee033ff76964fc770fb76fc
infiniteinference <ojas@infrhq.com>
2023-09-18 21:02:06 +0530

    Update: add search logic

--- app/page.js
+++ app/page.js
@@ -4,6 +4,7 @@ import { useEffect, useState } from 'react'
 import { telemetry } from '@/utils'
 import FloatingSearch from '@/components/Search'
 import Header from '@/components/Header'
+import Image from 'next/image'
 
 export default function Home() {
     const [serverUrl, setServerUrl] = useState('')
@@ -28,7 +29,7 @@ export default function Home() {
             <Header />
 
             <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-                <image
+                <Image
                     className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70]"
                     src="/replay.svg"
                     alt="Replay Logo"
--- components/SearchBar.js
+++ components/SearchBar.js
@@ -1,81 +1,121 @@
-import React, { useState } from 'react'
-import DatePicker from "react-datepicker";
-import "react-datepicker/dist/react-datepicker.css";
+"use client"
+
+import React, { useEffect, useState, useRef } from 'react'
+import DatePicker from 'react-datepicker'
+import 'react-datepicker/dist/react-datepicker.css'
+
+function SingleCard({ item, isLoading = false }) {
+    let dateGenerated
+    if (isLoading) {
+        dateGenerated = new Date()
+    } else {
+        dateGenerated = new Date(item?.date_generated)
+    }
+    dateGenerated = dateGenerated.toLocaleString()
+
+    let misc_item =item?.attributes?.window_name
+    if (item?.attributes?.current_url) {
+        misc_item = item?.attributes?.current_url
+    }
+    return (
+        <div
+            key={item?.name}
+            className={
+                'flex items-center p-4 border-b last:border-b-0 cursor-pointer' +
+                (isLoading ? ' animate-pulse' : '')
+            }
+        >
+            <img
+                src={item?.screenshot_url}
+                alt={item?.attributes?.app_name}
+                className="w-36 h-24 rounded-m mr-3"
+            />
+            <div className="flex-grow">
+                <h4 className="text-lg">
+                    {item?.attributes?.app_name}{' '}
+                    <span className="text-sm text-gray-500">
+                        {misc_item}
+                    </span>
+                </h4>
+                <p className="text-sm text-gray-600">{item?.name}</p>
+                <p className="text-xs text-gray-400">
+                    {dateGenerated}
+                </p>
+            </div>
+        </div>
+    )
+}
 
 function SearchBar() {
+    const [searchLoading, setSearchLoading] = useState(false)
     const [query, setQuery] = useState('')
     const [results, setResults] = useState([])
-    const [startDate, setStartDate] = useState(new Date(0,0,0));
-    const [endDate, setEndDate] = useState(new Date());
-    const data = [
-        {
-            image: 'path/to/image1.jpg',
-            name: 'John Doe',
-            nickname: 'Johnny',
-            description: 'A graphic designer from NY.',
-            date: '2023-01-01',
-        },
-        {
-            image: 'path/to/image1.jpg',
-            name: 'John Doe',
-            nickname: 'Johnny',
-            description: 'A graphic designer from NY.',
-            date: '2023-01-01',
-        },
-        {
-            image: 'path/to/image1.jpg',
-            name: 'John Doe',
-            nickname: 'Johnny',
-            description: 'A graphic designer from NY.',
-            date: '2023-01-01',
-        },
-        {
-            image: 'path/to/image1.jpg',
-            name: 'John Doe',
-            nickname: 'Johnny',
-            description: 'A graphic designer from NY.',
-            date: '2023-01-01',
-        },
-        {
-            image: 'path/to/image1.jpg',
-            name: 'John Doe',
-            nickname: 'Johnny',
-            description: 'A graphic designer from NY.',
-            date: '2023-01-01',
-        },
-        {
-            image: 'path/to/image1.jpg',
-            name: 'John Doe',
-            nickname: 'Johnny',
-            description: 'A graphic designer from NY.',
-            date: '2023-01-01',
-        },
-        {
-            image: 'path/to/image1.jpg',
-            name: 'John Doe',
-            nickname: 'Johnny',
-            description: 'A graphic designer from NY.',
-            date: '2023-01-01',
-        },
-        {
-            image: 'path/to/image1.jpg',
-            name: 'John Doe',
-            nickname: 'Johnny',
-            description: 'A graphic designer from NY.',
-            date: '2023-01-01',
-        },
-        // ... Add more data as needed
-    ]
+    const [startDate, setStartDate] = useState(new Date(1971, 0, 0))
+    const [endDate, setEndDate] = useState(new Date())
 
-    const handleSearch = (e) => {
+    const serverUrl = useRef('')
+    const apiKey = useRef('')
+    const searchDebounce = useRef(null)
+
+    useEffect(() => {
+        serverUrl.current = window.localStorage.getItem('serverUrl')
+        apiKey.current = window.localStorage.getItem('apiKey')
+    }, [])
+
+    const constructURL = () => {
+        let minTimeAsEpoch = startDate.getTime() / 1000
+        let maxTimeAsEpoch = endDate.getTime() / 1000
+        let IQL_QUERY = `
+        USE IQLV1.0.1
+        FILTER status EQUAL TO 'active'
+        VECTOR SEARCH '${query}'
+        FILTER date_generated GREATER THAN ${minTimeAsEpoch}
+        FILTER date_generated LESS THAN ${maxTimeAsEpoch}
+        ORDER BY date_generated DESC
+        LIMIT 10
+        FIELDS id, attributes, name, extracted_text, date_generated
+        MAKE screenshot_url
+        RETURN
+        `
+        let base_64_query = Buffer.from(IQL_QUERY).toString('base64')
+        return `${serverUrl.current}/v1/segment/query?query=${base_64_query}`
+    }
+
+    async function fetchData() {
+        setSearchLoading(true)
+        const url = constructURL()
+        const options = {
+            headers: {
+                'Infr-API-Key': apiKey.current,
+            },
+        }
+        const resp = await fetch(url, options)
+        const resp_json = await resp.json()
+        const new_segments = resp_json
+        new_segments.reverse()
+        setSearchLoading(false)
+        return new_segments
+    }
+
+    const handleSearch = async (e) => {
         const value = e.target.value
         setQuery(value)
+
         if (value) {
-            const filteredData = data.filter(
-                (item) =>
-                    item.name.includes(value) || item.nickname.includes(value),
-            )
-            setResults(filteredData)
+            // Clear any existing timeouts to prevent unnecessary searches
+            if (searchDebounce.current) {
+                clearTimeout(searchDebounce.current)
+            }
+
+            // Set a new timeout for the search
+            searchDebounce.current = setTimeout(async () => {
+                if (value) {
+                    let data = await fetchData()
+                    setResults(data)
+                } else {
+                    setResults([])
+                }
+            }, 3000) // 300 ms delay
         } else {
             setResults([])
         }
@@ -122,8 +162,11 @@ function SearchBar() {
                                 <path d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z" />
                             </svg>
                         </div>
-                        <DatePicker selected={startDate} onChange={(date) => setStartDate(date)} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full  p-4  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"/>
-
+                        <DatePicker
+                            selected={startDate}
+                            onChange={(date) => setStartDate(date)}
+                            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full  p-4  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                        />
                     </div>
                     <span class="mx-4 text-gray-500">to</span>
                     <div class="relative">
@@ -138,39 +181,28 @@ function SearchBar() {
                                 <path d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z" />
                             </svg>
                         </div>
- 
-                            <DatePicker selected={endDate} onChange={(date) => setEndDate(date)} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full  p-4  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"/>
+
+                        <DatePicker
+                            selected={endDate}
+                            onChange={(date) => setEndDate(date)}
+                            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full  p-4  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                        />
                     </div>
                 </div>
             </div>
-            {results.length > 0 && (
-                <div className="max-h-96 overflow-y-auto absolute w-full mt-2 bg-black bg-opacity-40 border border-gray-300 rounded-lg shadow-md z-10">
+            {results.length > 0 && !searchLoading && (
+                <div className="max-h-96 overflow-y-auto absolute w-full mt-2 bg-black  border border-gray-300 rounded-lg shadow-md z-10">
                     {results.map((item) => (
-                        <div
-                            key={item.name}
-                            className="flex items-center p-4 border-b last:border-b-0 cursor-pointer"
-                        >
-                            <img
-                                src={item.image}
-                                alt={item.name}
-                                className="w-12 h-12 rounded-full mr-3"
-                            />
-                            <div className="flex-grow">
-                                <h4 className="text-lg">
-                                    {item.name}{' '}
-                                    <span className="text-sm text-gray-500">
-                                        ({item.nickname})
-                                    </span>
-                                </h4>
-                                <p className="text-sm text-gray-600">
-                                    {item.description}
-                                </p>
-                                <p className="text-xs text-gray-400">
-                                    {item.date}
-                                </p>
-                            </div>
-                        </div>
+                        <SingleCard item={item} />
                     ))}
+                </div>
+            )}{' '}
+            {searchLoading && (
+                <div className="max-h-96 overflow-y-auto absolute w-full mt-2 bg-black bg-opacity-80 border border-gray-300 rounded-lg shadow-md z-10">
+                    <SingleCard isLoading={true} />
+                    <SingleCard isLoading={true} />
+                    <SingleCard isLoading={true} />
+                    <SingleCard isLoading={true} />
                 </div>
             )}
         </div>
